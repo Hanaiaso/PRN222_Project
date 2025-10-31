8ebf1ca4444ae6179637b285b475abf84ab7b69d
Hanaiaso <huy26072004@gmail.com>
2025-10-31 20:32:41 +0700

    add dashboard

---
+++ PRN_Project/PRN_Project/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN_Project.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            ViewData["Title"] = "Trang chủ học sinh";
+            return View();
+        }
+
+        public IActionResult Exams() => View();
+        public IActionResult Results() => View();
+        public IActionResult Profile() => View();
+    }
+}
