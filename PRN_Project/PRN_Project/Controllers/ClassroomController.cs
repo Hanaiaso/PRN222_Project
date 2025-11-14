f353f0a9703ea63e09411b3885a0032575c35145
linhmaideptrai <duylinh2992004@gmail.com>
2025-11-14 18:54:50 +0700

    class

---
+++ PRN_Project/PRN_Project/Controllers/ClassroomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN_Project.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
