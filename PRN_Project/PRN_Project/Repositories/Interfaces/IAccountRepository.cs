fd77258d5818449cc6895750d72b26c1c14febd6
Dvdung2 <dangvandungjsjo@gmail.com>
2025-11-15 18:37:47 +0700

    fix account teacher

--- PRN_Project/PRN_Project/Repositories/Interfaces/IAccountRepository.cs
+++ PRN_Project/PRN_Project/Repositories/Interfaces/IAccountRepository.cs
@@ -1,15 +1,33 @@
-﻿using PRN_Project.Models;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using PRN_Project.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PRN_Project.Repositories.Interfaces
 {
     public interface IAccountRepository
     {
+        // === Dùng cho Login/Register ===
+        Task<Account> GetByEmailAsync(string email);
+        Task<bool> EmailExistsAsync(string email);
+
+        // === Dùng chung ===
+        Task<Account> GetByIdAsync(int id);
+        Task AddAsync(Account account);
+        void Update(Account account); // Update/Remove thường là sync
+        void Remove(Account account);
+        Task<int> SaveChangesAsync();
+
         Account GetByEmail(string email);
         Account GetById(int id);
+
+
+
         bool EmailExists(string email);
         void Add(Account account);
-        void Update(Account account);
         void SaveChanges();
+
+        // === Dùng cho Quản lý Học sinh (Student Management) ===
+        Task<List<Account>> GetStudentAccountsWithDetailsAsync();
     }
 }
